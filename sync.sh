70bdc7b8d7ef8f8998b6935a2152446e43651658
Kaio Piola <35163593+kaiopiola@users.noreply.github.com>
2021-02-23 12:06:06 -0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -11,7 +11,7 @@ for n in $(cat $list)
 do
 	echo "Currently running for $n..."
 	filename="$n.log"
-	imapsync --host1 $ipfrom --port1 143 --user1 $n --password1 '$pass' --host2 $ipto --port2 143 --user2 $n --password2 '$pass' --syncinternaldates --noauthmd5 --split1 100 --split2 100 --sep1 . --prefix1 INBOX. --regextrans2 "s,/,-,g" --sep2 . --prefix2 INBOX. | tee logs/"$filename"
+	imapsync --host1 $ipfrom --port1 143 --user1 $n --password1 $pass --host2 $ipto --port2 143 --user2 $n --password2 $pass --syncinternaldates --noauthmd5 --split1 100 --split2 100 --sep1 . --prefix1 INBOX. --regextrans2 "s,/,-,g" --sep2 . --prefix2 INBOX. | tee logs/"$filename"
 done
 
 #END OF SCRIPT
